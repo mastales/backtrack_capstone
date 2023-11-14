91271f287cd7c84cbb9c69326c652baf425e1ec3
Lester Blake <lstr.mail@gmail.com>
2023-11-13 21:01:45 -0500

    completed comment controller logic. Will need to review tomorrow as some tables need to updated to accept new data

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,28 +1,73 @@
+const knex = require('../../db');
 // Controller for comments
 
 // Get all comments for a question card
 const getCommentsByQuestionCard = async (req, res) => {
-    const { qc_id } = req.params;
-    // Logic to retrieve comments and related info from the database based on qc_id
-    // Placeholder logic
+  const { qc_id } = req.params;
+
+  try {
+    // Check if the question card exists
+    const cardExists = await knex('question_cards').where('qc_id', qc_id).first();
+    if (!cardExists) {
+      return res.status(404).send('Question card not found');
+    }
+
+    // Fetch comments for the question card
+    const comments = await knex('comments').where('qc_id', qc_id).select();
+
     res.json({
       id: qc_id,
-      comments: [
-        // ... array of comment objects with user and track details
-      ]
+      comments: comments
     });
-  };
+  } catch (error) {
+    console.error('Error fetching comments:', error);
+    res.status(500).send('An error occurred while fetching comments.');
+  }
+};
+
   
   // Add a comment to a question card
   const addCommentToQuestionCard = async (req, res) => {
     const { qc_id } = req.params;
-    // Extract additional information from the request body if necessary
-    // Placeholder logic to add a comment
-    res.status(201).json({
-      message: `Comment added to question card with id ${qc_id}`,
-      // ... return the new comment object
-    });
+    const { user_id, content, track_name, artist_name, track_url, track_image_url } = req.body;
+  
+    try {
+      // Check if the question card exists
+      const cardExists = await knex('question_cards').where('qc_id', qc_id).first();
+      if (!cardExists) {
+        return res.status(404).send('Question card not found');
+      }
+  
+      const [newCommentId] = await knex('comments').insert({
+        qc_id,
+        user_id,
+        content,
+        track_name,
+        artist_name,
+        track_url,
+        track_image_url
+      });
+  
+      res.status(201).json({
+        message: 'Comment added successfully',
+        comment: {
+          comment_id: newCommentId,
+          qc_id,
+          user_id,
+          content,
+          comment_date: new Date().toISOString(),
+          track_name,
+          artist_name,
+          track_url,
+          track_image_url
+        }
+      });
+    } catch (error) {
+      console.error('Error adding comment:', error);
+      res.status(500).send('An error occurred while adding the comment.');
+    }
   };
+  
   
 // Function to add a comment and song to a question card
 const postCommentWithSong = async (req, res) => {
@@ -45,10 +90,28 @@ const postCommentWithSong = async (req, res) => {
     });
   };
   
-
+  const deleteComment = async (req, res) => {
+    const { comment_id } = req.params;
+  
+    try {
+      const deletedRows = await knex('comments').where('comment_id', comment_id).del();
+  
+      if (deletedRows) {
+        res.status(200).json({ message: 'Comment deleted successfully' });
+      } else {
+        res.status(404).send('Comment not found');
+      }
+    } catch (error) {
+      console.error('Error deleting comment:', error);
+      res.status(500).send('An error occurred while deleting the comment.');
+    }
+  };
+  
+  
   module.exports = {
     getCommentsByQuestionCard,
     addCommentToQuestionCard,
     postCommentWithSong,
+    deleteComment
   };
   
